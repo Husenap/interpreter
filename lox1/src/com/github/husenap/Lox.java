459b7017aed9bc44d050fcca1a51b193e8dbad4f
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2022-02-10 23:24:51 +0100

    fixed prompt

--- lox1/src/com/github/husenap/Lox.java
+++ lox1/src/com/github/husenap/Lox.java
@@ -34,7 +34,7 @@ public class Lox {
     BufferedReader reader = new BufferedReader(input);
 
     while (true) {
-      System.out.println("> ");
+      System.out.print("\n> ");
       String line = reader.readLine();
       if (line == null)
         break;
